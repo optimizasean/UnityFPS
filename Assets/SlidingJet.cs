9bdfe1dd1aba210d64b7de5cbb7d2487de8e7c19
Shadowbomb <shadowbomb20@gmail.com>
2019-02-04 12:17:18 -1000

    Completed Chapter 4 in book

---
+++ Assets/SlidingJet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidingJet : MonoBehaviour {
+    public float speed = 3.0f;
+    public float maxZ = 16.0f;
+    public float minZ = -16.0f;
+
+    private int _direction = 1;
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+
+        bool bounced = false;
+        if (transform.position.z > maxZ || transform.position.z < minZ) {
+            _direction = -_direction;
+            bounced = true;
+        }
+        if (bounced) {
+            transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+        }
+    }
+}
